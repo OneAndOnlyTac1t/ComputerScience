f20031bc539e84667c8e24626cff6ea0b0950871
OneAndOnlyTac1t <tacit1801@gmail.com>
2020-02-18 21:44:07 +0200

    done

---
+++ Laby/ComputerSystem1.2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerSystem1._2
+{
+    class Program
+    {
+        static readonly char[] base64Table = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+                                              'P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d',
+                                              'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+                                              't','u','v','w','x','y','z','0','1','2','3','4','5','6','7',
+                                              '8','9','+','/','=' };
+
+        static string Read(string path) //Метод зчитує текст з файлу
+
+        {
+            string filetext;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                filetext = sr.ReadToEnd();
+
+            }
+            return filetext;
+        }
+        static double[] Probability(string path) //Метод підраховує відносну частоту появи символу
+        {
+            string filetext = Read(path);
+            double[] probability = new double[65];
+            double[] counter = new double[65];
+            for (int m = 0; m < base64Table.Length; m++)
+            {
+                for (int i = 0; i < filetext.Length; i++)
+                {
+                    if (filetext[i] == base64Table[m])
+                    {
+                        counter[m]++;
+
+                    }
+
+                }
+                probability[m] = counter[m] / (filetext.Length + 1.0);
+
+            }
+            return probability;
+        }
+        static double AmountOfInformationn(double enthropy, string path) //Метод підраховує значення кількості інформації в тексті
+        {
+            return enthropy * AllSymbols(path);
+        }
+        static double AllSymbols(string path) //Метод підраховує загальну кількість символів у файлі
+        {
+            string filetext = Read(path);
+            return filetext.Length + 1.0;
+
+        }
+        static double AverageEnthropy(double[] probability) //Метод підраховує середню ентропію нерівноймовірного алфавіту
+        {
+            double averageEnthropy = 0;
+            for (int i = 0; i < probability.Length; i++)
+            {
+                if (probability[i] > 0)
+                {
+                    averageEnthropy += (probability[i] * Math.Log(probability[i], 2));
+
+                }
+            }
+            return (averageEnthropy * -1);
+        }
+        static void Base64Convert(string path)
+        {
+            string text;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                text = sr.ReadToEnd();
+            }
+            byte[] textBytes = Encoding.UTF8.GetBytes(text);
+            string binaryTextBytes = string.Join("", textBytes.Select(x => Convert.ToString(x, 2).PadLeft(8, '0')));
+            int countOfBytes = binaryTextBytes.Count();
+            string append = countOfBytes % 3 == 2 ? "==" : countOfBytes % 3 == 1 ? "=" : "";
+            int remOfDivision = countOfBytes % 6;
+            if (remOfDivision != 0)
+                for (int i = 0; i < 6 - remOfDivision; i++)
+                {
+                    binaryTextBytes = binaryTextBytes.Insert(countOfBytes, "0");
+                    countOfBytes++;
+                }
+            List<string> newList = Enumerable.Range(0, countOfBytes / 6).Select(x => binaryTextBytes.Substring(x * 6, 6)).ToList();
+            string myEnText = string.Join("", newList.Select(x => base64Table[Convert.ToByte(x, 2)])) + append;
+            Console.WriteLine("Текст закодований методом:\n" + myEnText + "\n");
+        }
+        static void Base64(string path)
+        {
+            string text;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                text = sr.ReadToEnd();
+            }
+            byte[] textBytes = Encoding.UTF8.GetBytes(text);
+            string enText = Convert.ToBase64String(textBytes);
+            using (StreamWriter sw = new StreamWriter(path.Replace(".bz2", "_base64.txt"), false))
+            {
+                sw.WriteLine(enText);
+            }
+            Console.WriteLine("За допомогою вбудованої бібліотеки:\n" + enText + "\n\n\n");
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Base64Convert(@"D:\Bogdan\1.tar.bz2");
+            Base64(@"D:\Bogdan\1.tar.bz2");
+            Console.WriteLine(AmountOfInformationn(AverageEnthropy(Probability(@"D:\Bogdan\1.tar_base64.txt")), @"D:\Bogdan\1.tar_base64.txt"));
+            Base64Convert(@"D:\Bogdan\2.tar.bz2");
+            Base64(@"D:\Bogdan\2.tar.bz2");
+            Console.WriteLine(AmountOfInformationn(AverageEnthropy(Probability(@"D:\Bogdan\2.tar_base64.txt")), @"D:\Bogdan\2.tar_base64.txt"));
+            Base64Convert(@"D:\Bogdan\3.tar.bz2");
+            Base64(@"D:\Bogdan\3.tar.bz2");
+            Console.WriteLine(AmountOfInformationn(AverageEnthropy(Probability(@"D:\Bogdan\3.tar_base64.txt")), @"D:\Bogdan\3.tar_base64.txt"));
+        }
+        
+    }
+}
---
+++ Laby/ComputerSystems1/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerSystems1
+{
+    internal class Program
+    {
+        static string Read(string path) //Метод зчитує текст з файлу
+
+        {
+            string filetext;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                filetext = sr.ReadToEnd();
+
+            }
+            return filetext;
+        }
+
+        static double[] Probability(string path) //Метод підраховує відносну частоту появи символу
+        {
+            string filetext = Read(path);
+            double[] probability = new double[65];
+            double[] counter = new double[65];
+            char[] symbol = { 'А', 'а', 'Б', 'б', 'В', 'в', 'Г', 'г', 'Ґ', 'ґ', 'Д', 'д', 'Е', 'е', 'Є', 'є', 'Ж', 'ж', 'З', 'з', 'И', 'и', 'І', 'і', 'Ї', 'ї', 'Й', 'й', 'К', 'к', 'Л', 'л', 'М', 'м', 'Н', 'н', 'О', 'о', 'П', 'п', 'С', 'р', 'С', 'с', 'Т', 'т', 'У', 'у', 'Ф', 'ф', 'Х', 'х', 'Ц', 'ц', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ', 'ь', 'Ю', 'ю', 'Я', 'я' };
+            for (int m = 0; m < symbol.Length; m++)
+            {
+                for (int i = 0; i < filetext.Length; i++)
+                {
+                    if (filetext[i] == symbol[m])
+                    {
+                        counter[m]++;
+
+                    }
+
+                }
+                probability[m] = counter[m] / (filetext.Length + 1.0);
+
+            }
+            return probability;
+        }
+
+        static double AverageEnthropy(double[] probability) //Метод підраховує середню ентропію нерівноймовірного алфавіту
+        {
+            double averageEnthropy = 0;
+            for (int i = 0; i < probability.Length; i++)
+            {
+                if (probability[i] > 0)
+                {
+                    averageEnthropy += (probability[i] * Math.Log(probability[i], 2));
+
+                }
+            }
+
+            return (averageEnthropy * -1);
+
+        }
+        static double AllSymbols(string path) //Метод підраховує загальну кількість символів у файлі
+        {
+            string filetext = Read(path);
+            return filetext.Length + 1.0;
+
+        }
+
+        static double AmountOfInformationn(double enthropy, string path) //Метод підраховує значення кількості інформації в тексті
+        {
+            return enthropy * AllSymbols(path);
+        }
+        static void AmountOut(double amount, string path) //Виводить порівняння кількості інформації в бітах з загальним розміром файлу
+        {
+            FileInfo file = new FileInfo(path);
+            double size = file.Length;
+            Console.WriteLine("Кількість інформації в тексті - {0} біт\nЗагальний розмір файлу - {1} біт\nРозмір файлу в - {2} рази більший за кількість інформації", amount, size * 8, size * 8 / amount);
+        }
+        static void ProbabilityOut(double[] probability) //Виводить у консоль символ та відносну частоту його появи
+        {
+            char[] symbol = { 'А', 'а', 'Б', 'б', 'В', 'в', 'Г', 'г', 'Ґ', 'ґ', 'Д', 'д', 'Е', 'е', 'Є', 'є', 'Ж', 'ж', 'З', 'з', 'И', 'и', 'І', 'і', 'Ї', 'ї', 'Й', 'й', 'К', 'к', 'Л', 'л', 'М', 'м', 'Н', 'н', 'О', 'о', 'П', 'п', 'С', 'р', 'С', 'с', 'Т', 'т', 'У', 'у', 'Ф', 'ф', 'Х', 'х', 'Ц', 'ц', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ', 'ь', 'Ю', 'ю', 'Я', 'я' };
+            Console.WriteLine("Відносна частота появи символу ");
+            for (int i = 0; i < symbol.Length; i++)
+            {
+                Console.WriteLine("{0,-5}{1}", symbol[i], probability[i]);
+            }
+        }
+        static void EnthropyOut(double enthropy) //Виводить у консоль середню ентропію
+        {
+            Console.WriteLine("Середня ентропія нерівноймовірного алфавіту\n{0}", enthropy);
+
+        }
+        static void AllOut(double amount, string path, double[] probability, double enthropy) //Виводить у консоль значення частот, ентропії та кількості інформації
+        {
+            ProbabilityOut(probability);
+            EnthropyOut(enthropy);
+            AmountOut(amount, path);
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string path1file = @"D:\Bogdan\1.tar_base64.txt";
+            string path2file = @"D:\Bogdan\2.tar_base64.txt";
+            string path3file = @"D:\Bogdan\1.tar_base64.txt";
+            Console.WriteLine("Перший файл");
+            double[] probability1 = Probability(path1file);
+            double enthropy1 = AverageEnthropy(probability1);
+            double amount1 = AmountOfInformationn(enthropy1, path1file);
+            AllOut(amount1, path1file, probability1, enthropy1);
+            Console.WriteLine("Другий файл");
+            double[] probability2 = Probability(path2file);
+            double enthropy2 = AverageEnthropy(probability2);
+            double amount2 = AmountOfInformationn(enthropy2, path2file);
+            AllOut(amount2, path2file, probability2, enthropy2);
+            Console.WriteLine("Третій файл");
+            double[] probability3 = Probability(path3file);
+            double enthropy3 = AverageEnthropy(probability3);
+            double amount3 = AmountOfInformationn(enthropy3, path3file);
+            AllOut(amount3, path3file, probability3, enthropy3);
+    
+        }
+    }
+}
